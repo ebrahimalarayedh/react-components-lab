09c5c017e009eb544c501553e8238b1bc7045c31
ebrahimalarayedh <ibrahim.mhd@hotmail.com>
2025-02-06 18:32:38 +0300

    done without the seperation

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,19 @@
+import './WeatherForecast.css'
+
+function WeatherForecasts(props){
+    let {day, img, imgAlt, conditions, time} = props
+
+    return (
+        <>
+            <div className="weather">
+                <h2>{day}</h2>
+                <img src={img} alt={imgAlt} />
+                <p><span>conditions: </span>{conditions}</p>
+                <p><span>time: </span>{time}</p>
+            </div>
+
+        </>
+    )
+}
+
+export default WeatherForecasts
